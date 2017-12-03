28e5a38bc0c17ba7091ed1dad81c3076348c361e
Harshana Madusanka Jayamaha <jmhmjayamaha@gmail.com>
2017-12-04 05:19:34 +0530

    adding car.json file

--- app/car-list/car-list.component.js
+++ app/car-list/car-list.component.js
@@ -1,11 +1,19 @@
 angular.module("carList").
 component("carList", {
 	templateUrl:"car-list/car-list.template.html",
-	controller: function CarListController() {
-		this.cars = [
-			{model:'212V', name:'axia', img_url:"img/axia_1.jpg"},
-			{model:'212V', name:'Maruti', img_url:"img/axia_2.jpg"},
-			{model:'212V', name:'Honda', img_url:"img/axiyo_1.jpg"}
-		]
-	}
+	controller: ['$http', function CarListController($http) {
+		this.car = [];
+
+		$http.get('car/car-list.json').then(function(response) {
+			this.cars =response.data;
+
+			console.log(this.cars);
+
+		});	
+		// this.cars = [
+		// 	{model:'212V', name:'axia', img_url:"img/axia_1.jpg"},
+		// 	{model:'212V', name:'Maruti', img_url:"img/axia_2.jpg"},
+		// 	{model:'212V', name:'Honda', img_url:"img/axiyo_1.jpg"}
+		// ]
+	}]
 });
