dcc8efc9daf949ae57dfbec1b587d9276822d1a9
Harshana Madusanka Jayamaha <jmhmjayamaha@gmail.com>
2017-12-03 18:45:52 +0530

    add car images

--- app/app.config.js
+++ app/app.config.js
@@ -1,37 +1,18 @@
 'use strict'
 
-// angular.module('myApp')
-// .config(['$locationProvider', '$routeProvider', function config($routeProvider, $locationProvider) {
+angular.module('myApp')
+.config(['$locationProvider', '$routeProvider', function config($locationProvider, $routeProvider) {
 
-// 	$locationProvider.hashPrefix('!');
+	$locationProvider.hashPrefix('!');
 
-// 	$routeProvider
-// 	.when("/cars", {
-// 		template: '<car-list></car-list>'
-// 	})
-// 	.when('/carsdetails', {
-// 		template: '<car-details></car-details>'
-// 	})
-// 	.otherwise('/cars')
-// } 
+	$routeProvider
+	.when("/cars", {
+		template: '<car-list></car-list>'
+	})
+	.when('/carsdetails', {
+		template: '<car-details></car-details>'
+	})
+	.otherwise('/cars')
+}
 
-// ]);
-
-
-
-angular.
-  module('myApp').
-  config(['$locationProvider' ,'$routeProvider',
-    function config($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix('!');
-
-      $routeProvider.
-        when('/cars', {
-          template: '<car-list></car-list>'
-        }).
-        when('/carsdetails', {
-          template: '<cars-details></cars-details>'
-        }).
-        otherwise('/cars');
-    }
-  ]);
+]);
--- app/car-list/car-list.component.js
+++ app/car-list/car-list.component.js
@@ -2,10 +2,10 @@ angular.module("carList").
 component("carList", {
 	templateUrl:"car-list/car-list.template.html",
 	controller: function CarListController() {
-		this.cars = [ 
-			{model:'212V', name:'axia'},
-			{model:'212V', name:'Maruti'},
-			{model:'212V', name:'Honda'}
+		this.cars = [
+			{model:'212V', name:'axia', img_url:"img/axia_1.jpg"},
+			{model:'212V', name:'Maruti', img_url:"img/axia_2.jpg"},
+			{model:'212V', name:'Honda', img_url:"img/axiyo_1.jpg"}
 		]
 	}
 });
